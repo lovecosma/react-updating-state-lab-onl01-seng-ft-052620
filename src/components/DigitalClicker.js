36070a3a25162005896e3f44df5e520de4a2b419
lovecosma <williamsmatthew333@gmail.com>
2020-09-26 12:15:56 -0600

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,28 @@
-// Code DigitalClicker Component Here
+import React, { Component } from 'react'
+
+export class DigitalClicker extends Component {
+
+    constructor(){
+        super()
+        this.state = {
+            timesClicked : 0
+        }
+    }
+
+    handleClick = (event) => {
+        let currentNumberOfClicks = this.state.timesClicked
+        this.setState({
+            timesClicked : currentNumberOfClicks += 1
+        })
+    }
+
+    render() {
+        return (
+            <div>
+               <button onClick={this.handleClick} >{this.state.timesClicked}</button> 
+            </div>
+        )
+    }
+}
+
+export default DigitalClicker
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,47 @@
-// Code YouTubeDebugger Component Here
+import React, {Component} from 'react'
+
+export default class YouTubeDebugger extends Component {
+
+    constructor(){
+        super()
+        this.state = {
+            errors: [],
+            user: null,
+            settings: {
+            bitrate: 8,
+            video: {
+            resolution: '1080p'
+                }
+            }
+        }
+    }
+
+    changeBitrate = (event) => {
+       this.setState({
+           settings : {
+           ...this.state.settings,
+           bitrate:  12
+           }
+       })
+    }
+
+    changeResolution = (e) => {
+        this.setState({
+            settings: {
+                ...this.state.settings,
+                video: {
+                    resolution: '720p'
+                }
+            }
+        })
+    }
+
+    render(){
+         return(
+             <div>
+                 <button className='bitrate' onClick={this.changeBitrate}>{this.state.settings.bitrate}</button>
+                 <button className='resolution' onClick={this.changeResolution}>{this.state.settings.video.resolution}</button>
+             </div>
+         )
+    }
+}
